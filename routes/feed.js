efdf9b49787337eaade4543799f5e87e7404efaa
Harsh Mule <hmuley22@gmail.com>
2020-08-31 16:22:27 +0530

    installed the mongoose package

--- routes/feed.js
+++ routes/feed.js
@@ -1,8 +1,12 @@
-const express  = require('express');
+const express = require('express');
+const { body } = require('express-validator/check');
 const router = express.Router();
 const feedcontroller = require('../controllers/feed');
 
 router.get('/posts', feedcontroller.getPosts);
 
-router.post('/post', feedcontroller.createPOsts);
+router.post('/post', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+], feedcontroller.createPOsts);
 module.exports = router;

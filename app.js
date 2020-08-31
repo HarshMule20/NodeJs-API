2002ae3ddb89a3a1b9da7de096b4b991f7ebfb06
Harsh Mule <hmuley22@gmail.com>
2020-08-31 16:21:25 +0530

    connection string to mongodb'[

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const feedroutes = require("./routes/feed");
 app.use(bodyparser.json()); //application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Origin', '*');   // This is to allow any host to call the server
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');  // This is to allow the specific methods to use by the server and client
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // This is the authentication part of the server and allow to send the content type!
     next();
 });
 

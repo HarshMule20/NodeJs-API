300e7a16a54aaf03a0b257780ec218d34a5b892d
Harsh Mule <hmuley22@gmail.com>
2020-08-31 16:22:06 +0530

    added the server side validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,9 +1,17 @@
+const { validationResult } = require('express-validator/check')
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
+                 _id: '1',
                 title: 'First Post',
-                content: 'This is the first post!'
+                content: 'This is the first post!',
+                imageURl: 'images/image1.png',
+                creator: {
+                    name: 'Harsh'
+                },
+                createdAt: new Date()
             }
         ]
     });
@@ -11,14 +19,25 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPOsts = (req, res, next) => {
     // Create post in DB
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        })
+    }
     const title = req.body.title;
     const content = req.body.content;
     res.status(201).json({
         message: 'Post create successfully!',
         post: {
-            id: new Date().toISOString(),
+            _id: new Date().toISOString(),
             title : title,
-            content : content 
+            content : content,
+            creator : {
+                name: 'HarshMule',
+            },
+            createdAt : new Date()
         }
     });
 };

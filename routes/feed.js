f137bace9a1baa34b063aacfb60c3401bfe95781
Harsh Mule <hmuley22@gmail.com>
2020-09-20 12:32:39 +0530

    add the auth in the routes

--- routes/feed.js
+++ routes/feed.js
@@ -2,21 +2,22 @@ const express = require('express');
 const { body } = require('express-validator/check');
 const router = express.Router();
 const feedcontroller = require('../controllers/feed');
+const isauth = require('../middleware/is-auth');
 
-router.get('/posts', feedcontroller.getPosts);
+router.get('/posts', isauth, feedcontroller.getPosts);
 
 router.post('/post', [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 })
-], feedcontroller.createPosts);
+], isauth, feedcontroller.createPosts);
 
-router.get('/post/:postId/', feedcontroller.getonePost);
+router.get('/post/:postId/', isauth, feedcontroller.getonePost);
 
 router.put('/post/:postId', [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 })
-], feedcontroller.updatePost);
+], isauth, feedcontroller.updatePost);
 
-router.delete('/post/:postId', feedcontroller.deletePost);
+router.delete('/post/:postId', isauth, feedcontroller.deletePost);
 
 module.exports = router;

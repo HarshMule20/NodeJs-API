733c1ca0fb42eb65d8229b1e27eed508da62716f
Harsh Mule <hmuley22@gmail.com>
2020-09-20 12:31:34 +0530

    added the authentication and token for the APIs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator/check');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
@@ -9,7 +11,61 @@ exports.signup = (req, res, next) => {
         error.data = errors.array();
         throw error;
     }
+    console.log("in the signup page")
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
+    bcryptjs.hash(password, 12)
+        .then(hashedPw => {
+            const user = new User({
+                email: email,
+                password: hashedPw,
+                name: name
+            });
+            return user.save()
+        })
+        .then(result => {
+            console.log(result)
+            res.status(201).json({ message: 'User created successfully', userId: result._id });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('User with this email cannot be found!');
+                error.statusCode = 404;
+                throw error;
+            }
+            loadedUser = user;
+            return bcryptjs.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password entered');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            }, 'bevhebrvhbehrvbevhrejv', { expiresIn: '1h' });
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 };
